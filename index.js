18f67c020c80bf8438e1999432e1b5d190730cc4
DavidG159 <gaspardavid235@gmail.com>
2025-04-19 10:32:04 -0600

    third commit

--- index.js
+++ index.js
@@ -8,6 +8,99 @@ window.addEventListener('DOMContentLoaded',() =>{
     let currentPlayer = 'X';
     let isGameActive = true;
 
+    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE = 'TIE';
+
+    /*
+
+    Indexes within the board
+    [0] [1] [2]
+    [3] [4] [5]
+    [6] [7] [8]
+
+
+    */
+
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 3, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+
+
+    function handleResultValidation() {
+        let roundWon = false;
+        for(let i = 0; i <=7; i++){
+            const winCondition = winningConditions[i];
+            const a = board[winCondition[0]];
+            const b = board[winCondition[1]];
+            const c = board[winCondition[2]];
+
+            if(a === '' || b === '' || c=== ''){
+                continue;
+            }
+            if(a === b && b === c){
+                roundWon = true;
+                break;
+            }
+        }
+
+        if(roundWon){
+            announce(curentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+            isGameActive = false;
+            return;
+        }
+
+        if(!board.includes(''))
+            announce(TIE);
+    }
+
+
+    const announce = (type) => {
+
+        switch(type){
+            case PLAYERO_WON:
+                announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
+                break;
+            case PLAYERX_WON:
+                announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
+                break;
+            case TIE:
+                announcer.innerText = 'Tie';
+        }
+        announcer.classList.remove('hide');
+    };
+
+
+
+    const changePlayer = () => {
+        playerDisplay.classList.remove(`player${currentPlater}`);
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        playerDisplay.innerText = currentPlayer;
+        playerDisplay.classList.add(`player${currentPlayer}`);
+    }
+
+
+    const userAction = (tile, index) => {
+        if(isValidAction(tile) && isGameActive){
+            tile.innerText = currentPlayer;
+            tile.classList.add(`player${currentPlayer}`);
+            updateBoard(index);
+            handleResultValidation();
+            changePlayer();
+        }
+    }
+
+    tiles.forEach( (tile, index) => {
+        tile.addEventListener('click',() => UserActivation(tile, index));
+    });
+
 
     resetButton.addEventListener('click',resetBoard);
 })
